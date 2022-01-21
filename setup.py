b16ffbe09d86749293f073009dc3c043c06e4a2b
Nicholas Hoffs <nic.hoffs@gmail.com>
2022-01-20 16:35:56 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Deriving complex NBA stats from SportsVU tracking data for the 2015-16 season',
+    author='Nicholas Hoffs',
+    license='MIT',
+)
